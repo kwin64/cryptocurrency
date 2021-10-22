491269c13588dd5420abd38869736a93e5a2ead9
kwin64 <kwin11bvb@yandex.by>
2021-10-22 14:40:18 +0300

    table for data with coincap api

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,12 @@ type DataType = {
 
 function App() {
 
-    const [data, setData] = useState<DataType>()
+    const [data, setData] = useState<Array<DataType>>()
 
     useEffect(() => {
         // @ts-ignore
         let id = 'bitcoin'
-        instance.get(`assets/${id}`)
+        instance.get(`assets`)
             // @ts-ignore
             .then(res => setData(res.data.data))
     }, [])
@@ -32,24 +32,35 @@ function App() {
     console.log(data)
 
     return (
-        <table className={'container'}>
-            <tr className={'title'}>
-                <th className={'line'}>#</th>
-                <th className={'line'}>Coin</th>
-                <th className={'line'}></th>
-                <th className={'line'}>Price $</th>
-                <th className={'line'}>24h Volume</th>
-                <th className={'line'}>Mkt Cap</th>
-            </tr>
-            <tr>
-                <th>{data?.id.length}</th>
-                <th>{data?.name}</th>
-                <th>{data?.symbol}</th>
-                <th>{data?.priceUsd}</th>
-                <th>{data?.volumeUsd24Hr}</th>
-                <th>{data?.marketCapUsd}</th>
-            </tr>
-        </table>
+        <>{data?.map(s => {
+            return (
+                <table className={'container'}>
+                    <tr className={'title'}>
+                        <th className={'line'}>Rank</th>
+                        <th className={'line'}>Name</th>
+                        <th className={'line'}>Price $</th>
+                        <th className={'line'}>Market Cap</th>
+                        <th className={'line'}>VWAP (24Hr) $</th>
+                        <th className={'line'}>Supply</th>
+                        <th className={'line'}>Volume (24Hr)</th>
+                        <th className={'line'}>Change (24Hr)</th>
+                    </tr>
+                    <tr>
+                        <th>{s.rank}</th>
+                        <th>{s.name}</th>
+                        <th>{Math.floor(+(s.priceUsd) * 100) / 100}</th>
+                        <th>{Math.floor(+(s.marketCapUsd) * 0.0000001) / 100}b</th>
+                        <th>{Math.floor(+(s.vwap24Hr) * 100) / 100}</th>
+                        <th>{Math.floor(+(s.supply) / 10000) / 100}m</th>
+                        <th>{Math.floor(+(s.volumeUsd24Hr) / 1000000) / 100}m</th>
+                        <th>{Math.floor(+(s.changePercent24Hr) * 100) / 100}%</th>
+                    </tr>
+                </table>
+            )
+        })}
+
+        </>
+
     );
 }
 
